eedb73e180c857139182b7149d26490cb9ff939e
williamlox <williamlox@yahoo.com.tw>
2020-12-14 21:13:58 +0800

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title> William 期末作業 </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -16,7 +16,7 @@
 
         <style>
             body {
-                font-family: 'Nunito';
+                font-family: '微軟正黑體', 'Nunito';
             }
         </style>
     </head>
